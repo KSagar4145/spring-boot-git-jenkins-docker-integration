26d43aafd835f69a6b7344658edcb8f64cc9fd95
Sagar Khachane <sagarpkhachane@gmil.com>
2024-01-29 22:35:29 +0530

    Springboot git jenkins docker integration first commit

---
+++ src/main/java/com/app/SpringBootGitJenkinsDockerApplication.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitJenkinsDockerApplication.class, args);
+		System.out.println("===================>>>>>SpringBootGitJenkinsDockerApplication is Up & Running");
+	}
+
+}
